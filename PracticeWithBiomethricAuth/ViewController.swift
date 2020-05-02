df51fe63a097325c6792ccc8b1aec04f9f862ea0
Cahyanto Setya Budi <cahyantosetyabudi@gmail.com>
2020-05-02 22:27:11 +0700

    Final

--- PracticeWithBiomethricAuth/ViewController.swift
+++ PracticeWithBiomethricAuth/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        authTouchID()
     }
 
+    func authTouchID() {
+        let localAuth = LAContext()
+        localAuth.localizedFallbackTitle = "Please use your passcode"
+
+        var authError: NSError?
+        let reason = "Authentication required to access the secure data"
+        
+        if localAuth.canEvaluatePolicy(.deviceOwnerAuthentication, error: &authError) {
+            localAuth.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { (success, evaluateError) in
+                if success {
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Success", message: "Authenticated succesfully", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                } else {
+                    //Failed to authenticate
+                    guard let error = evaluateError else {
+                        return
+                    }
+                    print(error)
+                }
+            }
+        } else {
+            guard let error = authError else {
+                return
+            }
+            print(error)
+        }
+    }
 
 }
 
