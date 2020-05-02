faf854cd6d719a3f7e47d93d387b48fef5616b4f
Cahyanto Setya Budi <cahyantosetyabudi@gmail.com>
2020-05-02 22:02:44 +0700

    Initial Commit

---
+++ PracticeWithBiomethricAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeWithBiomethricAuth
+//
+//  Created by Cahyanto Setya Budi on 02/05/20.
+//  Copyright © 2020 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
